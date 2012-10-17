af8a052ce1046ead7df2dfd2aadde02533b86131
Charlie Gonzalez <itcharlie@gmail.com>
2012-10-17 09:26:59 -0400

    err was editing file and forgot to save changes

--- lib/Billy/Handler/Clients.pm
+++ lib/Billy/Handler/Clients.pm
@@ -25,9 +25,7 @@ any ['get', 'post'] => '/add' => sub{
 
 any ['get', 'post'] => '/save' => sub {
 	my $params = params();
-    # check required fields.
-    my $field_check = Billy::Model::Clients->check_required_fields($params);
-    die $field_check unless $field_check == 1;
+
     # update client row.
     
     # return confirmation message.	
