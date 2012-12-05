aa74b80c6eac65f49d7658baf4f0c7d606f33552
Charlie Gonzalez <itcharlie@gmail.com>
2012-12-04 22:05:28 -0500

    Invoice.js updates
    - remove unecessary js code.
    - updated invoice.js to include all events in one function.
    - remove confirm button in invoice_edit template.

--- public/js/invoice.js
+++ public/js/invoice.js
@@ -1,49 +1,17 @@
 $(document).ready( function() {
-    $('.add_item').click( function(){
-        var $list =  $('#invoice-description table tr').length;
-        var $item_num = $list++;
-        $('#invoice-description table').append('<tr class="item"><td><button type="button" id="confirm_item">confirm</button></td><td><input type="text" size="10" id="item_quantity" name="item_'+$item_num+'_quantity" /></td><td><textarea type="text" cols="75" rows="2" name="item_'+$item_num+'_description" id="item_description"></textarea></td><td><input type="text" name="item_'+$item_num+'_price" id="item_price"/></td><td><input id="item_total" readonly="readonly" /></td></tr> '); 
-        add_events();
-    });
-
-    
-    $('.save_invoice').click( function(){
-        console.log($('#invoice-description').find(':hidden, :text, textarea').serialize());
-        $.ajax({
-            url:'/invoice/create',
-            type:'POST',
-            data:$('#invoice-description').find(':hidden, :text, textarea').serialize(),
-            dataType:"html",
-            success: function(message) {
-                alert(message);
-            },
-            error: function(){
-              alert( 'An error occured');
-            }
-            
-        })
-    });
-
-    add_events();
-    
+       events();
 
 });
 
-function add_events() {
-    $('.item #confirm_item').on("click",function(){
-        console.log($(this).parents('.item').find('input[type="text"], textarea') );
-        $(this).parents('.item').find(':text, textarea').attr('readonly', 'readonly');
-        
-    });
+function events() {
     
     $('.item #item_quantity').keyup( function() {
         $item_quantity = $(this).val();
-        console.log("Item Quantity: " + $item_quantity);
 	$item_price = $(this).parents('.item').find('#item_price').val();
-        console.log("Item Price: " + $item_price);
                
 	if( $item_price < 0 ) {
-            console.log("Nothing is calculated");
+            // console.log("Nothing is calculated");
+           // Add code to display zero 
 	} else if ($item_price >= 1 ) {
             $total_amount = $item_price * $item_quantity;
             $(this).parents('.item').find('#item_total').val($total_amount);
@@ -53,12 +21,11 @@ function add_events() {
     
     
         $('.item #item_price').keyup( function() {
-        $item_price = $(this).val();
-	$item_quantity = $(this).parents('.item').find('#item_quantity').val();
-         console.log("Item Price: " + $item_price);
-         console.log("Item Quantity: " + $item_quantity);
+            $item_price = $(this).val();
+            $item_quantity = $(this).parents('.item').find('#item_quantity').val();
 	if( $item_quantity < 0 ) {
             console.log("Nothing is calculated");
+           // Add code to display zero 
 	} else if ($item_quantity >= 1 ) {
             $total_amount = $item_price * $item_quantity;
             $(this).parents('.item').find('#item_total').val($total_amount.toFixed(2));
@@ -66,6 +33,34 @@ function add_events() {
 	};
         
     });
+
+
+     $('.add_item').click( function(){
+            var $list =  $('#invoice-description table tr').length;
+            var $item_num = $list++;
+            $('#invoice-description table').append('<tr class="item"><td><button type="button" id="confirm_item">confirm</button></td><td><input type="text" size="10" id="item_quantity" name="item_'+$item_num+'_quantity" /></td><td><textarea type="text" cols="75" rows="2" name="item_'+$item_num+'_description" id="item_description"></textarea></td><td><input type="text" name="item_'+$item_num+'_price" id="item_price"/></td><td><input id="item_total" readonly="readonly" /></td></tr> '); 
+            add_events();
+     });
+    
+        
+     $('.save_invoice').click( function(){
+        // console.log($('#invoice-description').find(':hidden, :text, textarea').serialize());
+         $.ajax({
+             url:'/invoice/create',
+             type:'POST',
+             data:$('#invoice-description').find(':hidden, :text, textarea').serialize(),
+             dataType:"html",
+             success: function(message) {
+                 alert(message);
+             },
+             error: function(){
+               alert( 'An error occured');
+             }
+             
+         })
+     });
+
+
 }
  
 function invoice_total(){
@@ -79,3 +74,4 @@ function invoice_total(){
     $('#invoice_total').val($invoice.toFixed(2));
     $invoice = 0;
 }
+
